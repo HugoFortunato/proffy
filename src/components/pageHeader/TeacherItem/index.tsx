c202e7be929a62b2317ddabc3886be2340655719
HugoFortunato <hp.fortunato@gmail.com>
2020-08-05 20:29:02 -0300

    final commit

--- src/components/pageHeader/TeacherItem/index.tsx
+++ src/components/pageHeader/TeacherItem/index.tsx
@@ -1,76 +1,48 @@
 import React from 'react'
 import whatsappIcon from '../../../assets/images/icons/whatsapp.svg'
 import './styles.css'
+import api from '../../../services/api'
 
-function TeacherItem() {
+export interface Teacher {
+    avatar: string,
+    bio: string,
+    cost: number,
+    id: number,
+    name: string,
+    subject: string,
+    whatsapp: string
+}
+
+interface TeacherItemProps {
+    teacher: Teacher
+}
+
+const TeacherItem: React.FC<TeacherItemProps> = ({ teacher }) => {
+
+    function createNewConnection() {
+        api.post('connections', {
+            user_id: teacher.id,
+        })
+    }
     return (
         <article className="teacher-item">
                  <header>
-                     <img src="https://media-exp1.licdn.com/dms/image/C5603AQGc1Ht0UuDlHw/profile-displayphoto-shrink_400_400/0?e=1602115200&v=beta&t=viigJVxHKfLRlGaoYbuWXDU8cEEIacJJOteQF2U0fcA" />
+                     <img src={teacher.avatar} alt={teacher.name} />
                         <div>
-                            <strong>Hugo Fortunato</strong>
-                            <span>Química</span>
+                            <strong>{teacher.name}</strong>
+                            <span>{teacher.subject}</span>
                         </div>
                  </header>
-                 <p>
-                 Entusiasta das melhores tecnologias de química avançada.
-                    <br /><br />
-                Apaixonado por explodir coisas em laboratório e por mudar a vida das pessoas através de experiências. Mais de 200.000 pessoas já passaram por uma das minhas explosões.
-                 </p>
+                 <p>{teacher.bio}</p>
                  <footer>
                      <p>
                          Preço/Hora
-                         <strong>R$ 80,00</strong>
+                         <strong>R$ {teacher.cost}</strong>
                      </p>
-                     <button type="button">
+                     <a onClick={createNewConnection}href={`{https://wa.me/${teacher.whatsapp}`}>
                          <img src={whatsappIcon} alt="Whatsapp" />
                          Entrar em contato
-                     </button>
-                 </footer>
-                 <header>
-                     <img src="https://media-exp1.licdn.com/dms/image/C4D03AQFCB6aBNFNR0g/profile-displayphoto-shrink_800_800/0?e=1602115200&v=beta&t=bGAqjn8hH4cKjqjK3H1tuHgSuN2Ba2ezgS3XxDvFyCs" />
-                        <div>
-                            <strong>Marcelo Pereira</strong>
-                            <span>Educação Física</span>
-                        </div>
-                 </header>
-                 <p>
-                 Instrutor de Educação Física para iniciantes, minha missão de vida é levar saúde e contribuir para o crescimento de quem se interessar.
-                  <br /><br />
-                Comecei a minha jornada profissional em 2001, quando meu 
-                pai me deu dois alteres de 32kg com a seguinte condição: "Aprenda a fazer dinheiro com isso!"
-                </p>
-                 <footer>
-                     <p>
-                         Preço/Hora
-                         <strong>R$ 50,00</strong>
-                     </p>
-                     <button type="button">
-                         <img src={whatsappIcon} alt="Whatsapp" />
-                         Entrar em contato
-                     </button>
-                 </footer>
-                 <header>
-                     <img src="https://media-exp1.licdn.com/dms/image/C4D03AQGqtaeE5pg_rg/profile-displayphoto-shrink_400_400/0?e=1602115200&v=beta&t=aKR9UnOdC0C7IMhVwSt9vj2Qo7ICDvjWKq5D32SQtTU" />
-                        <div>
-                            <strong>Igor Sebek</strong>
-                            <span>Geografia</span>
-                        </div>
-                 </header>
-                 <p>
-                 As vezes não sei nem onde eu tô, mas consigo me localizar facilmente em qualquer lugar. 
-                 Tenho memória fotográfica e nunca fico perdido.
-                 Eu ensino a galera como não se perder na vida, com lições geográficas simples pra você nunca mais precisar de mapa na sua bela vida.
-                 </p>
-                 <footer>
-                     <p>
-                         Preço/Hora
-                         <strong>R$ 100,00</strong>
-                     </p>
-                     <button type="button">
-                         <img src={whatsappIcon} alt="Whatsapp" />
-                         Entrar em contato
-                     </button>
+                     </a>
                  </footer>
              </article>
     );
--- src/pages/landing/index.tsx
+++ src/pages/landing/index.tsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import logoImg from '../../assets/images/logo.svg'
 import landingImg from '../../assets/images/landing.svg'
 import studyIcon from '../../assets/images/icons/study.svg'
@@ -6,16 +6,29 @@ import giveClassesIcon from '../../assets/images/icons/give-classes.svg'
 import purpleHeartIcon  from '../../assets/images/icons/purple-heart.svg'
 import './styles.css'
 import { Link } from 'react-router-dom'
+import api from '../../services/api'
 
 
 
 function Landing() {
+
+    const [totalConnections, setTotalConnections] = useState(0)
+
+    useEffect(() => {
+        api.get('connections').then(response => {
+            const {total} = response.data
+
+            setTotalConnections(total)
+        })
+    }, [])
+
     return (
         <div id="page-landing">
             <div id="page-landing-content" className="container">
                 <div className="logo-container">
                     <img src={logoImg} alt="Proffy" />
                     <h2>Sua plataforma de estudos online.</h2>
+                    <div className="prefere"><h4>O que você prefere?</h4></div>
                 </div>
                 <img src={landingImg} alt="Plataforma de Estudos" className="hero-image" />
                 <div className="buttons-container">
@@ -28,7 +41,7 @@ function Landing() {
                         Dar aulas
                     </Link>
                 </div>
-                <span className="total-connections">Total de 200 conexões já realizadas <img src={purpleHeartIcon} alt="Coração Roxo" /></span>
+                <span className="total-connections">Total de {totalConnections} conexões já realizadas <img src={purpleHeartIcon} alt="Coração Roxo" /></span>
             </div>
         </div>
     )
